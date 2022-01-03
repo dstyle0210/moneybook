cac535d707be371f5a4cd143e4cfbf60d45a47b2
dstyle0210 <dstyle0210@gmail.com>
2022-01-03 16:26:08 +0900

    0103 정리

--- public/page/update.js
+++ public/page/update.js
@@ -8,5 +8,54 @@ $(function () {
     console.log();
     Receipts = snapshot.val();
     console.log(Receipts[url.searchParams.get("idx")]);
+    const $reactRoot = $("#receiptsUpdateForm");
+    ReactDOM.render( /*#__PURE__*/React.createElement(S_receiptsUpdate, {
+      receipt: Receipts[url.searchParams.get("idx")]
+    }), $reactRoot.get(0));
   });
 });
+
+const S_receiptsUpdate = ({
+  receipt
+}) => {
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("label", {
+    for: "write_dateTime"
+  }, "\uACB0\uC81C\uC2DC\uAC04"), /*#__PURE__*/React.createElement("input", {
+    type: "datetime-local",
+    id: "write_dateTime",
+    value: receipt.datetime
+  })), /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("label", {
+    for: "write_store"
+  }, "\uC0AC\uC6A9\uCC98"), /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    id: "write_store"
+  })), /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("label", {
+    for: "write_price"
+  }, "\uAE08\uC561"), /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    id: "write_price"
+  })), /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("label", {
+    for: "write_payment"
+  }, "\uACB0\uC81C\uC218\uB2E8"), /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    id: "write_payment"
+  })), /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("label", {
+    for: "write_comment"
+  }, "\uC0C1\uC138\uB0B4\uC5ED"), /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    id: "write_comment"
+  })), /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("label", null, "\uC9C0\uCD9C\uC131\uACA9"), /*#__PURE__*/React.createElement("label", null, /*#__PURE__*/React.createElement("input", {
+    type: "radio",
+    name: "write_outgoingsType"
+  }), " \uACE0\uC815"), /*#__PURE__*/React.createElement("label", null, /*#__PURE__*/React.createElement("input", {
+    type: "radio",
+    name: "write_outgoingsType"
+  }), " \uD544\uC218"), /*#__PURE__*/React.createElement("label", null, /*#__PURE__*/React.createElement("input", {
+    type: "radio",
+    name: "write_outgoingsType",
+    checked: true
+  }), " \uBCC0\uB3D9"), /*#__PURE__*/React.createElement("label", null, /*#__PURE__*/React.createElement("input", {
+    type: "radio",
+    name: "write_outgoingsType"
+  }), " \uAE30\uD0C0")));
+};
--- src/page/update/update.jsx
+++ src/page/update/update.jsx
@@ -6,5 +6,40 @@ $(function(){
         console.log();
         Receipts = snapshot.val();
         console.log( Receipts[url.searchParams.get("idx")] );
+
+        const $reactRoot = $("#receiptsUpdateForm");
+        ReactDOM.render( <S_receiptsUpdate receipt={Receipts[url.searchParams.get("idx")]} /> ,$reactRoot.get(0));
     });
 });
+
+
+const S_receiptsUpdate = ({receipt}) => {
+    return (<React.Fragment><div>
+        <label for="write_dateTime">결제시간</label>
+        <input type="datetime-local" id="write_dateTime" value={receipt.datetime} /> 
+    </div>
+    <div>
+        <label for="write_store">사용처</label>
+        <input type="text" id="write_store" />
+        
+    </div>
+    <div>
+        <label for="write_price">금액</label>
+        <input type="text" id="write_price" />
+    </div>
+    <div>
+        <label for="write_payment">결제수단</label>
+        <input type="text" id="write_payment" />
+    </div>
+    <div>
+        <label for="write_comment">상세내역</label>
+        <input type="text" id="write_comment" />
+    </div>
+    <div>
+        <label>지출성격</label>
+        <label><input type="radio" name="write_outgoingsType" /> 고정</label>
+        <label><input type="radio" name="write_outgoingsType" /> 필수</label>
+        <label><input type="radio" name="write_outgoingsType" checked /> 변동</label>
+        <label><input type="radio" name="write_outgoingsType" /> 기타</label>
+    </div></React.Fragment>);
+};
