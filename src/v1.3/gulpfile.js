da1feb4aa66c6d48e554f7752ba099b1975b310d
dstyle0210 <dstyle0210@gmail.com>
2022-03-17 17:38:35 +0900

    폴더만 정리해봄

--- src/v1.3/gulpfile.js
+++ src/v1.3/gulpfile.js
@@ -12,10 +12,16 @@ function defaultTask(cb) {
     cb();
 }
 
+/*! 변수 설정 */
 let version = "v1.3";
 let srcRoot = "../../src/"+version+"/pages";
 let distRoot = "../../dist/"+version;
 let buildRoot = "../../public/"+version;
+
+/*! 공통함수 */
+function getFolderName(watchPath,rootFolderName){
+    return (nodePath.parse(watchPath).dir).replace(rootFolderName+"\\",""); // 폴더명 구하기
+}
 
 task("page:scss",function(done){
     let globs = ["./pages/**/*.scss"];
@@ -27,7 +33,7 @@ task("page:scss",function(done){
         .pipe(dest(distRoot+"/"+folderName));
     };
     return watch(globs).on("change",function(path){
-        let folderName = (nodePath.parse(path).dir).replace("pages\\",""); // 폴더명 구하기
+        let folderName = getFolderName(path,"pages"); // 폴더명 구하기
         return build(folderName).on("end",function(){
             console.log(path);
         });
@@ -41,3 +47,28 @@ task("page:scss",function(done){
         console.log("pages:scss watch Ready");
     });
 });
+
+task("layout:scss",function(done){
+    let globs = ["./_layout/**/*.scss"];
+    function build(){
+        return src(globs)
+        .pipe(concat("layout.scss"))
+        .pipe(sass())
+        .pipe(dest(distRoot+"/asset/css"));
+    };
+    return watch(globs).on("change",function(path){
+        return build().on("end",function(){
+            console.log(path);
+        });
+    }).on("ready",function(){
+        build().on("end",function(){
+            done();
+            console.log("layout:scss watch Ready");
+        });
+    });
+});
+
+
+
+
+task("dev",series("page:scss"));
