31ac817d22b074e45f58fc283223d2d8f9219214
dstyle0210 <dstyle0210@gmail.com>
2023-11-03 16:32:14 +0900

    업데이트

--- dist/v1.2/asset/js/service.js
+++ dist/v1.2/asset/js/service.js
@@ -47,9 +47,9 @@ function getSmsDateTime(text){
     }
 };
 function getSmsMethod(text){
-    if( (/9\*4\*/).test(text) ){
+    if( (/9043/).test(text) ){
         return "국민봉올림";
-    }else if( (/0\*0\*/).test(text) ){
+    }else if( (/0805/).test(text) ){
         return "국민마올림";
     }else if( (/스마일카드/).test(text) ){
         return "현대스마일";
--- src/v1.2/_service/getSmsMethod.js
+++ src/v1.2/_service/getSmsMethod.js
@@ -1,7 +1,7 @@
 function getSmsMethod(text){
-    if( (/9\*4\*/).test(text) ){
+    if( (/9043/).test(text) ){
         return "국민봉올림";
-    }else if( (/0\*0\*/).test(text) ){
+    }else if( (/0805/).test(text) ){
         return "국민마올림";
     }else if( (/스마일카드/).test(text) ){
         return "현대스마일";
