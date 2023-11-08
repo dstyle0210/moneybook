8ecd2e3c20c1d4c8f8a7ab561a07b85087783418
dstyle0210 <dstyle0210@gmail.com>
2023-11-08 13:22:44 +0900

    마무리

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# moneybookTest_discard
-=> moneybookNextjs로 이관.
+# moneybook
+가계부 v1.28
+- 구글로그인 (firebase auth)
+- firebase DB , hosting 이용
+- 붙여넣기 : 국민카드 , 현대카드(네이버,스마일) 문자내역
+- 빠른입력(현금) 지원
+
+## 콘솔
+### firebase login을 위한 권한변경
+```powershell
+Set-ExecutionPolicy RemoteSigned
+```
+### Git 등록 (github)
+```console
+git config --global user.name dstyle0210
+git config --global user.email dstyle0210@gmail.com
+```
+
+### 필요모듈 설치
+```console
+npm i -g firebase-tools gulp-cli gulp
+npm i
+```
+
+### Dev (해당버전 이동)
+```console
+cd ./src/v1.2
+gulp dev
+gulp dist
+```
+
+
+### Firebase Deploy
+```console
+firebase login
+gulp deploy
+```
+
+### 네이버 가계부 자동등록
+- export 를 통해 json데이터 가져온 후 실행
+```console
+node naverExport
+```
--- naverExport.js
+++ naverExport.js
@@ -4,7 +4,8 @@ var result = [];
 var browser,page;
 (async () => {
     browser = await chromium.launch({
-        headless:false
+        headless:false,
+        executablePath:"C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
     });
     page = await browser.newPage();
 
@@ -127,12 +128,12 @@ var browser,page;
         return true;
     };
 
-    let data2 = [];
+    let data2 = [{"date":"2023.11.03","store":"태광상사주식회","comment":"휘발유 1665","method":"국민봉올림","price":"50000","tag":"변동,자동차,택시","type":"card"},{"date":"2023.11.01","store":"포도약국","comment":"이비인후과 약","method":"국민봉올림","price":"3900","tag":"변동,의료비","type":"card"},{"date":"2023.11.01","store":"참좋은이비인후과","comment":"왼쪽 고막 절개술","method":"국민봉올림","price":"29900","tag":"변동,의료비","type":"card"},{"date":"2023.11.01","store":"카카오 T 주차 ","comment":"디지털큐브 주차","method":"국민봉올림","price":"13900","tag":"변동,자동차,택시","type":"card"},{"date":"2023.11.01","store":"네이버페이\r","comment":"도진 지갑","method":"현대네이버","price":"8390","tag":"기타,그외,뭐지","type":"card"},{"date":"2023.11.03","store":"카카오 T 주차","comment":"다지털큐브","method":"국민봉올림","price":"13900","tag":"변동,자동차,택시","type":"card"},{"date":"2023.11.03","store":"하남정","comment":"해장국2 , 돈까스","method":"국민봉올림","price":"33000","tag":"변동,외식,커피","type":"card"},{"date":"2023.11.04","store":"예손약국","comment":"지르세틴","method":"국민봉올림","price":"3000","tag":"변동,의료비","type":"card"},{"date":"2023.11.04","store":"파리바게뜨(김포","comment":"소세지빵 ","method":"국민봉올림","price":"710","tag":"변동,외식,커피","type":"card"},{"date":"2023.11.04","store":"롯데슈퍼 김포전","comment":"도진 우유","method":"국민봉올림","price":"890","tag":"필수,음료수,간식","type":"card"},{"date":"2023.11.04","store":"김영화 부대찌개","comment":"김포페이 , 부대찌개2, 어린이불고기","method":"김포페이","price":"28000","tag":"변동,외식,커피","type":"cash"},{"date":"2023.11.04","store":"김포시청","comment":"공영주차비","method":"국민봉올림","price":"1800","tag":"변동,자동차,택시","type":"card"},{"date":"2023.11.04","store":"동보민나 주식회 팔각도","comment":"장인어른 장모님 외식","method":"국민봉올림","price":"117500","tag":"변동,외식,커피","type":"card"},{"date":"2023.11.04","store":"동보민나 주식회 팔각도","comment":"라면 추가","method":"국민봉올림","price":"13000","tag":"변동,외식,커피","type":"card"},{"date":"2023.11.05","store":"알리익스프레스","comment":"아버님 모니터","method":"국민봉올림","price":"39325","tag":"기타,그외,뭐지","type":"card"},{"date":"2023.11.05","store":"트레이더스","comment":"콤비피자, 음료","method":"현대스마일","price":"17800","tag":"변동,외식,커피","type":"card"},{"date":"2023.11.05","store":"트레이더스 식료품","comment":"냉동식품 외","method":"현대스마일","price":"152170","tag":"필수,주식,식재료","type":"card"},{"date":"2023.11.05","store":"JL마켓탑","comment":"식료품","method":"국민봉올림","price":"5700","tag":"필수,주식,식재료","type":"card"},{"date":"2023.11.05","store":"정이가마트","comment":"식료품, 계란 등","method":"국민봉올림","price":"13940","tag":"필수,주식,식재료","type":"card"},{"date":"2023.11.06","store":"카카오 T 주차","comment":"디지털큐브 주차","method":"국민봉올림","price":"13900","tag":"변동,자동차,택시","type":"card"},{"date":"2023.11.06","store":"주식회사위메프\r","comment":"도진 옷","method":"현대네이버","price":"9060","tag":"기타,미용,패션","type":"card"},{"date":"2023.11.07","store":"감탄떡볶이","comment":"떡볶이 1인분","method":"김포페이","price":"3500","tag":"변동,외식,커피","type":"cash"},{"date":"2023.11.07","store":"11번가-SK PAY","comment":"화장품","method":"국민봉올림","price":"19805","tag":"기타,미용,패션","type":"card"},{"date":"2023.11.07","store":"엄마손찬방","comment":"김밥","method":"국민봉올림","price":"6000","tag":"변동,외식,커피","type":"card"},{"date":"2023.11.07","store":"(주)프린팅박스","comment":"마용 여권사진","method":"국민봉올림","price":"2000","tag":"기타,그외,뭐지","type":"card"},{"date":"2023.11.07","store":"도로교통공단 안","comment":"마용운전면허갱신","method":"국민봉올림","price":"13800","tag":"기타,그외,뭐지","type":"card"},{"date":"2023.11.07","store":"김포감정주유소","comment":"휘발유 1645","method":"국민봉올림","price":"50000","tag":"변동,자동차,택시","type":"card"},{"date":"2023.11.07","store":"김포감정주유소","comment":"세차비","method":"국민봉올림","price":"5000","tag":"변동,자동차,택시","type":"card"},{"date":"2023.11.07","store":"예향정김포걸포","comment":"제육, 김치찌개, 새우볶음밥","method":"국민봉올림","price":"34000","tag":"변동,외식,커피","type":"card"},{"date":"2023.11.07","store":"참좋은이비인후","comment":"고믹진료, 독감주사","method":"국민봉올림","price":"44700","tag":"변동,의료비","type":"card"},{"date":"2023.11.07","store":"당근거래","comment":"흔한남매 6-10","method":"현금","price":"25000","tag":"변동,문화,여행","type":"cash"}];
     let data3 = JSON.stringify(data2);
     let data4 = JSON.parse(data3);
     let count = 0;
     for(let item of data4){
-        item.store = (item.store).repalce("\r","");
+        item.store = (item.store).replace("\r","");
         await test(item);
     };
 
